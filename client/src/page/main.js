c5c46de0c906ab0811b162a8e6f980f26b5d0691
beepbipbp <riskyjunk@gmail.com>
2022-12-31 03:04:12 +0900

    컴포넌트 함수형으로 변경

--- client/src/page/main.js
+++ client/src/page/main.js
@@ -1,37 +1,54 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import SectionApi from "../api/section.api.js";
 
-class Main extends React.Component {
-	state = {
-		sectionList: [],
+function Main() {
+	const navigate = useNavigate();
+	const [sectionList, setSectionList] = useState([]);
+
+	useEffect(() => {
+		const fetchSectionList = async () => {
+			const response = await SectionApi.getSectionList();
+			setSectionList(response);
+		};
+		fetchSectionList();
+	}, []);
+
+	const makeSectionList = () => {
+		const sectionListDOM = sectionList.map((section) => {
+			return (
+				<li key={section.section_id}>
+					<div
+						className="main__section_list"
+						onClick={() =>
+							navigate("business-simple?section=" + section.section_name)
+						}
+					>
+						{section.section_name}
+					</div>
+				</li>
+			);
+		});
+		return sectionListDOM;
 	};
 
-	async componentDidMount() {
-		const sectionList = await SectionApi.getSectionList();
-		this.setState({
-			sectionList,
-		});
-	}
-
-	makeSectionList() {
-		const sectionList = this.state.sectionList.map((section) => {
-			return <li key={section.section_id}>{section.section_name}</li>;
-		});
-		return sectionList;
-	}
-
-	render() {
-		return (
-			<Fragment>
-				<h1>Sections</h1>
-				<ul>
-					<li key="all">전체</li>
-					{this.makeSectionList()}
-				</ul>
-			</Fragment>
-		);
-	}
+	return (
+		<Fragment>
+			<h1>Sections</h1>
+			<ul>
+				<li key="all">
+					<div
+						className="main__section_list"
+						onClick={() => navigate("business-simple?section=all")}
+					>
+						전체
+					</div>
+				</li>
+				{makeSectionList()}
+			</ul>
+		</Fragment>
+	);
 }
 
 export default Main;
