77dec7c7932540869c577a499a7c1dce16789338
beepbipbp <riskyjunk@gmail.com>
2022-12-30 22:38:23 +0900

    client측 dotenv 설치

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
+import * as dotenv from "dotenv";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/index.scss";
+
+dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render();
