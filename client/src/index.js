24b347ce4e6b9ee4a8c1f5d8aff37e39f75c6175
beepbipbp <riskyjunk@gmail.com>
2022-12-30 23:03:31 +0900

    client측 react router dom 설치

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import * as dotenv from "dotenv";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import "./style/index.scss";
+import router from "./router.js";
 
 dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<RouterProvider router={router} />);
