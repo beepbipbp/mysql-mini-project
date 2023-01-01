0440e7956f34045b9224510dc0d50e154f17d48c
beepbipbp <riskyjunk@gmail.com>
2023-01-02 00:33:51 +0900

    business list 호출 API에서 query문 오류 수정

--- server/src/business/business.model.js
+++ server/src/business/business.model.js
@@ -3,7 +3,7 @@ import { businessColumnEnum } from "./business.enum.js";
 
 class BusinessModel {
 	static async getBusinessList(sectionName, floor, status, canTakeout, sortBy) {
-		const result = await dbConnection.execute(`
+		console.log(`
 			SELECT
 				B.business_id AS businessId,
 				B.business_name AS businessName,
@@ -23,6 +23,28 @@ class BusinessModel {
 				${canTakeout ? `B.can_takeout = ${canTakeout}` : ""}
 			JOIN sections S
 			  ON B.fk_section_id = S.section_id
+			${sortBy ? `ORDER BY ${businessColumnEnum[sortBy]}` : ""}
+		`);
+		const result = await dbConnection.execute(`
+			SELECT
+				B.business_id AS businessId,
+				B.business_name AS businessName,
+				S.section_name AS sectionName,
+				S.floor AS floor,
+				B.status AS status,
+				B.can_takeout AS canTakeout
+			FROM
+				businesses B
+			JOIN sections S
+			  ON B.fk_section_id = S.section_id
+			${sectionName || floor || status || canTakeout ? "WHERE" : ""}
+			  ${sectionName ? `S.section_name = "${sectionName}"` : ""}
+				  ${sectionName && (floor || status || canTakeout) ? "AND" : ""}
+				${floor ? `S.floor = ${floor}` : ""}
+				  ${floor && (status || canTakeout) ? "AND" : ""}
+				${status ? `B.status = "${status}"` : ""}
+				  ${status && canTakeout ? "AND" : ""}
+				${canTakeout ? `B.can_takeout = ${canTakeout}` : ""}
 			${sortBy ? `ORDER BY ${businessColumnEnum[sortBy]}` : ""}
 		`);
 
